ae0cd9be90816bb981ba7d1073c24b9bc44d4985
Matthew Sit <msit@berkeley.edu>
2016-11-24 16:11:54 -0800

    initial launch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,56 @@
 $(document).ready(function() {
+    /* Blinking down arrow on project pages */
+    var isTransparent = false;
 
+    var noScrollYet = setInterval(function() {
+        if (noScrollYet) {
+            if (isTransparent) {
+                $(".proj-summary img").css("filter","opacity(0)");
+                isTransparent = false;
+            } else {
+                $(".proj-summary img").css("filter","opacity(0.8)");
+                isTransparent = true;
+            }
+        }
+    }, 1000);
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 0) {
+            clearInterval(noScrollYet);
+            $(".proj-summary img").css("filter","opacity(0.8)");
+        }
+    });
+
+    /* Hide/Show Navbar on Scroll Down/Up */
+    var scroll = 0;
+
+    $(window).scroll(function() {
+        if (window.innerWidth >= 1000) {
+            if ($(window).scrollTop() > scroll) {
+                $(".navbar").hide();
+            } else {
+                $(".navbar").show();
+            }
+            scroll = $(window).scrollTop();
+        }
+    });
+
+    /* Hide/Show Navbar options when Menu Button is clicked (only applies to window width < 1000) */
+    var isShowNavbar = false;
+
+    $("#navbar-menu-btn").click(function(event) {
+        event.stopPropagation();
+        if (isShowNavbar) {
+            $(".navbar").hide();
+            isShowNavbar = false;
+        } else {
+            $(".navbar").show();
+            isShowNavbar = true;
+        }
+    });
+
+    $(window).click(function() {
+        $(".navbar").hide();
+        isShowNavbar = false;
+    });
 });
