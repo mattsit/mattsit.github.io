0d6db9d9211f7d272c58bb9e035458d17155dade
Matthew Sit <msit@berkeley.edu>
2017-07-01 19:53:01 -0700

    launching 61bl mini-lecture content

---
+++ assets/js/hiddenscript.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    /* Intially hide all lab content divs. */
+    var allDivs = document.querySelectorAll('.proj-content-container');
+    for (var i = 0; i < allDivs.length; i++) {
+        allDivs[i].style.display = 'none';
+    }
+    $(".proj-content-container").hide();
+
+    /* Set hand cursor for when hovering over any link. */
+    var allLinks = document.getElementsByClassName("proj-summary-detail");
+    for (var i = 0; i < allLinks.length; i++) {
+        allLinks[i].style.cursor = "pointer";
+    }
+
+    /* Clicking any link hides all lab content divs, then shows its corresponding content. */
+    $(".proj-summary-detail").click(function(event) {
+        for (var i = 0; i < allDivs.length; i++) {
+            allDivs[i].style.display = 'none';
+        }
+        $(".proj-content-container").hide();
+
+        var labNumber = $(event.target).text().trim().substring(4,6);
+        $("#" + labNumber).show();
+    });
+
+});
