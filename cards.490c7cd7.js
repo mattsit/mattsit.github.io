c6130777266cb3d1b4ffdf9a96550daaf1c3faff
Matthew Sit <msit@berkeley.edu>
2020-07-03 22:55:34 -0700

    Updates

--- cards.490c7cd7.js
+++ cards.490c7cd7.js
@@ -128,7 +128,7 @@ $(document).ready(function () {
     });
   });
 });
-},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -156,7 +156,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60336" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61170" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -332,5 +332,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","assets/js/cards.js"], null)
+},{}]},{},["../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","assets/js/cards.js"], null)
 //# sourceMappingURL=/cards.490c7cd7.js.map
--- navbar.66e62c2e.js
+++ navbar.66e62c2e.js
@@ -144,7 +144,7 @@ $(document).ready(function () {
     $("#navbar-menu-btn").css("filter", "none");
   });
 });
-},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -172,7 +172,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60336" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61170" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -348,5 +348,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","assets/js/navbar.js"], null)
+},{}]},{},["../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","assets/js/navbar.js"], null)
 //# sourceMappingURL=/navbar.66e62c2e.js.map
