0606fc4abe4d62ae60ad6b46ff7545608ed2439e
Matthew Sit <msit@berkeley.edu>
2020-07-12 14:27:06 -0700

    Require masonry as an npm module instead of relying on a script import.

--- assets/ts/cards.ts
+++ assets/ts/cards.ts
@@ -1,10 +1,23 @@
+var jQueryBridget = require('jquery-bridget/jquery-bridget');
+var ImagesLoaded = require('imagesloaded');
+var Masonry = require('masonry-layout');
+// Enable as jQuery plugins.
+jQueryBridget( 'masonry', Masonry, $ );
+jQueryBridget( 'imagesLoaded', ImagesLoaded, $ );
+
+// Type declaration for imagesLoaded.
+interface JQuery {
+    imagesLoaded: (callback) => any;
+    callback: () => void;
+}
+
 $(document).ready(function() {
-    var $cards = $('.proj-container').imagesLoaded( function() {
-      // init Masonry after all images have loaded
-      $cards.masonry({
+    var $cards = $('.proj-container').imagesLoaded(function() {
+        // init Masonry after all images have loaded
+        $cards.masonry({
           itemSelector: '.proj-card',
           gutter: '.proj-card-gutter',
           horizontalOrder: true
-      });
+        });
     });
 });
